67b5da05f0f6ccf29af78447d04b3cced2e48cca
= <jakcllns@gmail.com>
2018-05-26 16:08:14 -0400

    Post users input to the home page

--- functional_tests.py
+++ functional_tests.py
@@ -4,12 +4,13 @@ import time
 import unittest
 
 path = "E:\\Git Repository\\python-tdd-book\\Chrome Web Driver\\chromedriver.exe"
-
+ie_driver = "E:\\Git Repository\\python-tdd-book\\Internet Explorer Web Driver\\IEDriverServer.exe"
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome(path)
-
+        #self.browser = webdriver.Chrome(path)
+        self.browser = webdriver.Ie(ie_driver)
+        
     def tearDown(self):
         self.browser.quit()
 
@@ -42,20 +43,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         # The pages updates again, and now shows both items on her list
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
         # Edith wonders whether the site will remember her lst. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory test to that effect.
-
+        self.fail('Finish the test!')
         # She visists that URL - her to-do list is still there.
 
         #Satisfied, she goes back to sleep
