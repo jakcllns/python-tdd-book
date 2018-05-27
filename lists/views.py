433a04005519bba61f2c29bc62949da656b82d0b
= <jakcllns@gmail.com>
2018-05-26 20:02:11 -0400

    Two more migrations added List to lists/models.py

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
@@ -13,6 +10,7 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
     
