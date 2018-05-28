106ffbd6ad7c483c8740fbb241265437ede64ff2
= <jakcllns@gmail.com>
2018-05-28 00:00:42 -0400

    gitignore .env file

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = True
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME'].split('|')]
+    ALLOWED_HOSTS = os.environ['SITENAME'].split('|')
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
