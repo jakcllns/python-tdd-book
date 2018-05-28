71090c20d5f429eca766c934e2820e4ef31e8be9
= <jakcllns@gmail.com>
2018-05-28 07:37:49 -0400

    Added fabfile.py to deploy_tools

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -8,7 +8,7 @@ def deploy():
 	site_folder = f'/home/{env.user}/sites/{env.host}'
 	run(f'mkdir -p {site_folder}')
 	with cd(site_folder):
-		_get_lastest_source()
+		_get_latest_source()
 		_update_virtualenv()
 		_create_or_update_dotenv()
 		_update_static_files()
